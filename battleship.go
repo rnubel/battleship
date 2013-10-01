b53c2004a4940e29202e26c808d2523408139c68
Robert Nubel <rnubel@enovafinancial.com>
2013-10-01 00:22:00 -0500

    Refactors how hits are stored, extends API.

--- battleship.go
+++ battleship.go
@@ -5,24 +5,54 @@ type Coord struct {
 	y int
 }
 
+type ShipPart struct {
+  loc   Coord
+  isHit bool
+}
+
 type Ship struct {
-	startPos   Coord
-	horizontal bool
-	size       int
-	status     []bool
+  parts   []ShipPart
+  isDead  bool
 }
 
 type Board struct {
-	width  int
-	height int
+	Width  int
+	Height int
 	hits   []Coord
 	misses []Coord
 	ships  []Ship
 }
 
-func (s Ship) isHit(x, y int) bool {
-	return (s.horizontal && x < s.startPos.x+s.size && y == s.startPos.y ||
-		!s.horizontal && y < s.startPos.y+s.size && x == s.startPos.x)
+func (s *Ship) covers(x, y int) bool {
+  for i := range(s.parts) {
+    if s.parts[i].loc.x == x && s.parts[i].loc.y == y {
+      return true
+    }
+  }
+
+  return false
+}
+
+func (s *Ship) RecordShot(x, y int) bool {
+  for i := range(s.parts) {
+    if s.parts[i].loc.x == x && s.parts[i].loc.y == y {
+      s.parts[i].isHit = true
+      s.isDead = s.checkIfDestroyed()
+      return true
+		}
+  }
+
+  return false
+}
+
+func (s *Ship) checkIfDestroyed() bool {
+  for i := range(s.parts) {
+    if !s.parts[i].isHit {
+      return false
+    }
+  }
+
+  return true
 }
 
 func (b *Board) recordHit(x, y int) {
@@ -35,14 +65,16 @@ func (b *Board) recordMiss(x, y int) {
 	b.misses = append(b.misses, c)
 }
 
-func (b *Board) recordShot(x, y int) (hit bool, err string) {
-	if x < 0 || y < 0 || x >= b.width || y >= b.height {
+// Public API follows.
+
+func (b *Board) RecordShot(x, y int) (hit bool, err string) {
+	if x < 0 || y < 0 || x >= b.Width || y >= b.Height {
 		hit, err = false, "out_of_bounds"
 		return
 	}
 
 	for i := range b.ships {
-		hit = hit || b.ships[i].isHit(x, y)
+		hit = hit || b.ships[i].RecordShot(x, y)
 	}
 
 	if hit {
@@ -54,17 +86,68 @@ func (b *Board) recordShot(x, y int) (hit bool, err string) {
 	return
 }
 
-func (b *Board) placeShip(x, y, size int, horizontal bool) (ok bool, err string) {
-	c := Coord{x: x, y: y}
-	s := Ship{startPos: c, horizontal: horizontal, size: size}
+func (b *Board) HitAt(x, y int) bool {
+  for i := range(b.hits) {
+    if b.hits[i].x == x && b.hits[i].y == y {
+      return true
+    }
+  }
+  return false
+}
 
-	if x < 0 || y < 0 || (horizontal && x+size > b.width) || (!horizontal && y+size > b.height) {
+func (b *Board) MissAt(x, y int) bool {
+  for i := range(b.misses) {
+    if b.misses[i].x == x && b.misses[i].y == y {
+      return true
+    }
+  }
+  return false
+}
+
+func (b *Board) ShipAt(x, y int) bool {
+  for i := range(b.ships) {
+    if b.ships[i].covers(x, y) {
+      return true
+    }
+  }
+  return false
+}
+
+func (b *Board) NumShips() int {
+  return len(b.ships)
+}
+
+func (b *Board) PlaceShip(x, y, size int, horizontal bool) (ok bool, err string) {
+	s := Ship{}
+
+	if x < 0 || y < 0 || (horizontal && x+size > b.Width) || (!horizontal && y+size > b.Height) {
 		ok, err = false, "out_of_bounds"
 		return
 	}
+
+  for i := 0; i < size; i++ {
+    loc := Coord{x: x, y: y}
+    if horizontal {
+      loc.x += i
+    } else {
+      loc.y += i
+    }
+
+    part := ShipPart{loc: loc, isHit: false}
+    s.parts = append(s.parts, part)
+  }
 
 	b.ships = append(b.ships, s)
 
 	ok, err = true, ""
 	return
+}
+
+func (b *Board) IsLost() (lost bool) {
+  lost = true
+  for i := range(b.ships) {
+    lost = lost && b.ships[i].isDead
+  }
+
+  return
 }
--- battleship_test.go
+++ battleship_test.go
@@ -5,54 +5,75 @@ import (
 )
 
 func TestBoard(t *testing.T) {
-	b := Board{width: 10, height: 10}
+	b := Board{Width: 10, Height: 10}
 
-	if b.width != 10 || b.height != 10 {
-		t.Log("Height and width were not set correctly")
-		t.Fail()
-	}
-}
-
-func TestHits(t *testing.T) {
-	b := Board{width: 5, height: 5}
-
-  b.recordHit(0, 2)
-
-	if len(b.hits) != 1 {
-		t.Log("Hit was not added correctly.")
+	if b.Width != 10 || b.Height != 10 {
+		t.Log("Height and Width were not set correctly")
 		t.Fail()
 	}
 }
 
 func TestShips(t *testing.T) {
-  b := Board{width: 10, height: 10}
+  b := Board{Width: 10, Height: 10}
 
-  ok, err := b.placeShip(0, 0, 10, true)
+  ok, err := b.PlaceShip(0, 0, 10, true)
   if !ok || err != "" {
-    t.Log("Was not able to place a 10-size ship at 0,0")
-    t.Fail()
+    t.Error("Was not able to place a 10-size ship at 0,0")
   }
 
-  ok, err = b.placeShip(0, 0, 11, false)
+  ok, err = b.PlaceShip(0, 0, 11, false)
   if ok || err != "out_of_bounds" {
-    t.Log("Error was not returned properly")
-    t.Fail()
+    t.Error("Error was not returned properly")
   }
 }
 
 func TestHittingShips(t *testing.T) {
-  b := Board{width: 10, height: 10}
-  b.placeShip(0, 0, 10, true)
+  b := Board{Width: 10, Height: 10}
+  b.PlaceShip(0, 0, 10, true)
 
-  hit, _ := b.recordShot(4, 4)
+  hit, _ := b.RecordShot(4, 4)
   if hit {
-    t.Log("4, 4 returned 'hit' when it should not have")
-    t.Fail()
+    t.Error("4, 4 returned 'hit' when it should not have")
   }
 
-  hit, _ = b.recordShot(4, 0)
+  hit, _ = b.RecordShot(4, 0)
   if !hit {
-    t.Log("4, 0 returned 'miss' when it should not have")
-    t.Fail()
+    t.Error("4, 0 returned 'miss' when it should not have")
+  }
+}
+
+func TestDestroyingShips(t *testing.T) {
+  b := Board{Width: 10, Height: 10}
+  b.PlaceShip(0, 0, 2, true)
+
+  b.RecordShot(0,0)
+  b.RecordShot(1,0)
+
+  if !b.ships[0].isDead {
+    t.Error("A ship of 2 parts was not destroyed properly")
+  }
+
+
+  b.PlaceShip(3, 3, 1, true)
+
+  b.RecordShot(3,3)
+
+  if !b.ships[1].isDead {
+    t.Error("A ship of 1 part was not destroyed properly")
+  }
+}
+
+func TestLosingBoard(t *testing.T) {
+  b := Board{Width: 10, Height: 10}
+  b.PlaceShip(0, 0, 2, true)
+  b.PlaceShip(2, 2, 2, false)
+
+  b.RecordShot(0, 0)
+  b.RecordShot(1, 0)
+  b.RecordShot(2, 2)
+  b.RecordShot(2, 3)
+
+  if !b.IsLost() {
+    t.Error("Board is not lost even after all ships are destroyed")
   }
 }
